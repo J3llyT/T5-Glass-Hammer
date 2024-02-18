e17fc462f0494f47d8d9efc0c268ab641a11e076
Tanessa H <t.nessah0@outlook.com>
2024-02-18 00:16:41 -0500

    Repo Set Up
    Figured out how to properly set up the repo for modding. Refer to the discord for notes team :) I'm gonna go cry now

---
+++ Code/ModEntry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+
+namespace HopeToRiseMod
+{
+    public class ModEntry : Mod
+    {
+        /*
+         * We can override these two methods later. Or I can create a testing folder and give this class access to it sow
+         * we can get rid of them here. I'll put it on the back burner for now though.
+         */
+        /*********
+      ** Public methods
+      *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        }
+
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+
+            // print button presses to the console window
+            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+        }
+    }
+}
