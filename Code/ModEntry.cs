d30a5cbf5fd39de7ae095231484d84252a3cf235
Tanessa H <t.nessah0@outlook.com>
2024-02-21 21:41:57 -0500

    [CP] Quest Implementation
    The first quest to go to harvey's hospital is working. TODO: have quest complete after talking to Harvey instead of just visiting the clinic

--- Code/ModEntry.cs
+++ Code/ModEntry.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
+using QuestFramework.Api;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
+
 
 
 namespace HopeToRiseMod
@@ -14,6 +16,9 @@ namespace HopeToRiseMod
          * We can override these two methods later. Or I can create a testing folder and give this class access to it sow
          * we can get rid of them here. I'll put it on the back burner for now though.
          */
+        private HTRQuest questItems;
+        
+
         /*********
       ** Public methods
       *********/
@@ -21,21 +26,18 @@ namespace HopeToRiseMod
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            helper.Events.GameLoop.GameLaunched += this.OnGameStarted;
+            questItems = new HTRQuest();
+
         }
 
-        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event data.</param>
-        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        /// <summary>
+        //  Loads Quest when the game launches
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnGameStarted(object sender, GameLaunchedEventArgs e)
         {
-            // ignore if player hasn't loaded a save yet
-            if (!Context.IsWorldReady)
-                return;
-
-
-            // print button presses to the console window
-            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
         }
     }
 }
