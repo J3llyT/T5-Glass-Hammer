d2d12f087f83fb154d3845bbebfe1828b73a866f
Tanessa H <t.nessah0@outlook.com>
2024-02-24 13:56:00 -0500

    CP Quest x Events
    Added the first Act 2 Quest and event.

--- Code/ModEntry.cs
+++ Code/ModEntry.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
-using QuestFramework.Api;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
@@ -12,32 +11,32 @@ namespace HopeToRiseMod
 {
     public class ModEntry : Mod
     {
-        /*
-         * We can override these two methods later. Or I can create a testing folder and give this class access to it sow
-         * we can get rid of them here. I'll put it on the back burner for now though.
-         */
-        private HTRQuest questItems;
-        
-
-        /*********
-      ** Public methods
-      *********/
+      /*********
+        ** Public methods
+        *********/
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            helper.Events.GameLoop.GameLaunched += this.OnGameStarted;
-            questItems = new HTRQuest();
-
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
         }
 
-        /// <summary>
-        //  Loads Quest when the game launches
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnGameStarted(object sender, GameLaunchedEventArgs e)
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
         {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            // print button presses to the console window
+            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
         }
     }
 }
+
