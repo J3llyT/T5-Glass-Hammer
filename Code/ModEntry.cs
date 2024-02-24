6e905d54715ed62b71af4d1c2390608c499223de
Dominic Apgar <dominicapgar2100@gmail.com>
2024-02-24 14:27:20 -0500

    Integrated Slleep/ Stamina Warps
    Bug that prompts players to sleep upon 'waking up' from the dream

--- Code/ModEntry.cs
+++ Code/ModEntry.cs
@@ -1,40 +1,77 @@
 ﻿using System;
+using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
-
-
+using StardewValley.GameData.LocationContexts;
+using StardewValley.Locations;
+using StardewValley.Menus;
+using StardewValley.Tools;
 
 namespace HopeToRiseMod
 {
-    public class ModEntry : Mod
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
     {
-        /*
-         * We can override these two methods later. Or I can create a testing folder and give this class access to it sow
-         * we can get rid of them here. I'll put it on the back burner for now though.
-         */
-        
+        private int staminaThreshold = 3; // Adjust this value as needed
 
-        /*********
-      ** Public methods
-      *********/
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            helper.Events.GameLoop.GameLaunched += this.OnGameStarted;
+            helper.Events.GameLoop.UpdateTicked += OnUpdateTicked;
+            helper.Events.Display.MenuChanged += OnMenuChanged;
 
         }
 
-        /// <summary>
-        //  Loads Quest when the game launches
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnGameStarted(object sender, GameLaunchedEventArgs e)
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnUpdateTicked(object sender, EventArgs e)
         {
+            if (Game1.player.stamina <= staminaThreshold && Game1.currentLocation != null
+                && Game1.currentLocation.Name == "DreamWorldSpawn")
+            {
+                Monitor.Log("Player Passed Out!", LogLevel.Info);
+
+                // Display a blackout screen
+                Game1.showGlobalMessage("You're waking up...");
+
+                // Teleport the player to their last slept-in bed location (NEED TO FIND BED LOCATION)
+                Game1.warpHome();
+                // WarpPlayerToNewLocation("FarmHouse", (int)Game1.player.mostRecentBed.X, (int)Game1.player.mostRecentBed.Y);
+                // Game1.player.setTileLocation(Game1.player.mostRecentBed);
+
+
+                // Reset the player's stamina to prevent further passouts
+                Game1.player.stamina = Game1.player.MaxStamina;
+            }
+        }
+
+        private void OnMenuChanged(object sender, MenuChangedEventArgs e)
+        {
+            // Handles sleep warp logic.
+            if (Game1.player.isInBed.Value && Game1.currentLocation.lastQuestionKey != null && Game1.currentLocation.lastQuestionKey.StartsWith("Sleep"))
+            {
+                Game1.currentLocation.afterQuestion += SleepWarp;
+            }
+        }
+
+        // Warps player to new location.
+        private void WarpPlayerToNewLocation(string locationName, int xCoord, int yCoord)
+        {
+            Game1.warpFarmer(locationName, xCoord, yCoord, false);
+        }
+
+        // Checks to see if player clicked 'Yes' to sleep.
+        public void SleepWarp(Farmer who, string whichAnswer)
+        {
+            if (whichAnswer is "Yes")
+            {
+                WarpPlayerToNewLocation("dreamworldspawn", 4, 4);
+            }
         }
     }
 }
